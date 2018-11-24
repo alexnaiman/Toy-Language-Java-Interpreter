835083e2698a0599f63b10b2c6c8b137573d1fc1
Alexandru Naiman <alex.naiman.4@gmail.com>
2018-11-24 17:57:15 +0200

    boolean expressions added

---
+++ src/Model/Expression/BooleanExpression/LowerExpression.java
@@ -0,0 +1,26 @@
+package Model.Expression.BooleanExpression;
+
+import Model.Expression.IExpression;
+import Model.Utils.IDictionary;
+import Model.Utils.IHeap;
+
+public class LowerExpression implements IExpression {
+    private IExpression left, right;
+
+    public LowerExpression(IExpression left, IExpression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int eval(IDictionary<String, Integer> dict, IHeap<Integer, Integer> heap) {
+        if (left.eval(dict, heap) < right.eval(dict, heap))
+            return 1;
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left + "<" + right + ")";
+    }
+}
